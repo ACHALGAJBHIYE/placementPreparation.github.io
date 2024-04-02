c251ad86d07d427e2f6e252e133c28a9bd56bf4d
ACHAL GAJBHIYE <127335581+ACHALGAJBHIYE@users.noreply.github.com>
2024-04-02 18:24:20 +0530

    Add files via upload
    28-03-24                                                                                                                                                                                                                                                                                             Program to count the number of even and odd integers in a given array of integer.

---
+++ EvenOddCounter.java
@@ -0,0 +1,19 @@
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for (int num : array) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        
+        System.out.println("Number of even integers: " + evenCount);
+        System.out.println("Number of odd integers: " + oddCount);
+    }
+}
